26152fe06c6291f5ca0629fc8d9f35cd087aa04d
unknown <8mau.coronel@gmail.com>
2024-03-19 14:38:26 -0600

    Implementing post component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,42 +1,15 @@
 import { useState, useEffect } from 'react';
-import io from 'socket.io-client';
 
 import Header from '../Header';
+import Posts from '../Post';
 import { MainSection, PostingSection, PostingInput } from '../../styles/Main';
-
-import {
-  Post,
-  PostContainer,
-  PostInfo,
-  PostContent,
-  PostInteraction,
-  PostImg,
-  ButtonInteraction,
-} from '../../styles/Post';
-
-import {
-  CommentContainer,
-  CommentInfo,
-  CommentInput,
-  CommentText,
-  Comments,
-} from '../../styles/Comments';
 import PostModal from '../PostModal';
 
 export default function MainPage() {
   const [modal, setModal] = useState(false);
   const [posts, setPosts] = useState([]);
-  const [comments, setComments] = useState([]);
-  const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io('http://localhost:8000', { forceNew: true });
-    setSocket(newSocket);
-
-    newSocket.on('likesCount', (dataRecived) => {
-      console.log(dataRecived);
-    });
-
     const jwt = JSON.parse(localStorage.getItem('token'));
 
     const config = {
@@ -48,18 +21,12 @@ export default function MainPage() {
 
     fetch('http://localhost:8000/user/follow-posts', config)
       .then((res) => res.json())
-      .then((res) => setPosts(res))
+      .then((res) => setPosts(res.finalPosts))
       .catch((err) => console.error(err));
   }, []);
 
   const toggleModal = (state) => {
     setModal(state);
-  };
-
-  const handleLike = (post_id) => {
-    const jwt = JSON.parse(localStorage.getItem('token'));
-    const data = { jwt, post_id };
-    socket.emit('like', data);
   };
 
   return (
@@ -71,35 +38,7 @@ export default function MainPage() {
           onClick={() => toggleModal(true)}
         ></PostingInput>
       </PostingSection>
-      <Post>
-        {posts.map((post) => {
-          return (
-            <PostContainer key={post.postId}>
-              <PostInfo>{post.user}</PostInfo>
-              <PostContent>{post.text}</PostContent>
-              <PostImg src={post.img} />
-              <PostInteraction>
-                <ButtonInteraction onClick={() => handleLike(post.postId)}>
-                  Like
-                </ButtonInteraction>
-                <ButtonInteraction>Comment</ButtonInteraction>
-              </PostInteraction>
-              <Comments>
-                {comments.map((comment) => {
-                  return (
-                    <CommentContainer key={comment.id}>
-                      <CommentInfo>{comment.user}</CommentInfo>
-                      <CommentText>{comment.text}</CommentText>
-                    </CommentContainer>
-                  );
-                })}
-              </Comments>
-              <CommentInput placeholder="Comment" />
-            </PostContainer>
-          );
-        })}
-      </Post>
-
+      <Posts sendData={posts} />
       {modal && <PostModal closeModal={toggleModal} />}
     </MainSection>
   );
---
+++ src/components/Post/index.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+
+import {
+  CommentContainer,
+  CommentInfo,
+  CommentInput,
+  CommentText,
+  Comments,
+} from '../../styles/Comments';
+import {
+  ButtonInteraction,
+  Post,
+  PostContainer,
+  PostContent,
+  PostImg,
+  PostInfo,
+  PostInteraction,
+} from '../../styles/Post';
+
+export default function Posts({ sendData }) {
+  const [socket, setSocket] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    setPosts(sendData);
+    console.log(sendData);
+
+    const newSocket = io('http://localhost:8000', { forceNew: true });
+    setSocket(newSocket);
+
+    newSocket.on('likesCount', (dataRecived) => {
+      console.log(dataRecived);
+    });
+  }, [sendData]);
+
+  const handleLike = (post_id) => {
+    const jwt = JSON.parse(localStorage.getItem('token'));
+    const data = { jwt, post_id };
+    socket.emit('like', data);
+  };
+
+  return (
+    <Post>
+      {posts.map((post) => {
+        return (
+          <PostContainer key={post.id}>
+            <PostInfo>{post.user}</PostInfo>
+            <PostContent>{post.content}</PostContent>
+            <PostImg src={post.img} />
+            <PostInteraction>
+              <ButtonInteraction onClick={() => handleLike(post.postId)}>
+                Like
+              </ButtonInteraction>
+              <ButtonInteraction>Comment</ButtonInteraction>
+            </PostInteraction>
+            <Comments>
+              {post.comments.map((comment) => {
+                return (
+                  <CommentContainer key={comment.id}>
+                    <CommentInfo>{comment.user}</CommentInfo>
+                    <CommentText>{comment.content}</CommentText>
+                  </CommentContainer>
+                );
+              })}
+            </Comments>
+            <CommentInput placeholder="Comment" />
+          </PostContainer>
+        );
+      })}
+    </Post>
+  );
+}
